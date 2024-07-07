2dcc949e137b35f57b2d913ce677dfe9f86f36a9
Kaikai0522 <106466501+Kaikai0522@users.noreply.github.com>
2024-07-07 20:39:36 +0800

    easy

--- 2582.cpp
+++ 2582.cpp
@@ -3,7 +3,7 @@ public:
     int passThePillow(int n, int time) {
         int move = 1, ans = 1;
         while(time--){
-            ans += move;
+            ans += move; 
             if(ans == n) move = -1;
             else if(ans == 1) move = 1;
         }

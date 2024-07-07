f7c0f729264890bd15da2e188d9443f47835c357
Kaikai0522 <106466501+Kaikai0522@users.noreply.github.com>
2024-07-07 20:39:21 +0800

    Create 2582.cpp

---
+++ 2582.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int move = 1, ans = 1;
+        while(time--){
+            ans += move;
+            if(ans == n) move = -1;
+            else if(ans == 1) move = 1;
+        }
+        return ans;
+    }
+};

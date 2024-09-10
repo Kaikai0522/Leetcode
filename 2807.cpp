f26f642b43e5fc5f2e41aa1d94b4b4b3aee4e5e6
Kaikai0522 <106466501+Kaikai0522@users.noreply.github.com>
2024-09-10 20:35:55 +0800

    Create 2807.cpp

---
+++ 2807.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* cur = head;
+        while(cur -> next != nullptr){
+            int _gcd = gcd(cur->val, cur->next->val);
+            ListNode *new_node = new ListNode(_gcd, cur -> next);
+            cur -> next = new_node;
+            cur = new_node -> next;
+        }
+        return head;
+    }
+};

6ecd77220e8e003cd528cf78fbb431099a111ddf
Kaikai0522 <106466501+Kaikai0522@users.noreply.github.com>
2024-10-04 16:39:36 +0800

    medium

---
+++ 2491.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        const int n = skill.size();
+        long long sum = 0;
+        vector<int> skill_hash(1001, 0);
+        for(int i = 0;i < n;++i){
+            sum += skill[i];
+            ++skill_hash[skill[i]];
+        }
+        int teams_number = n / 2, target = sum / teams_number;
+        long long ans = 0;
+        if(sum % teams_number != 0) return -1;
+        for(int i = 0;i < n;++i){
+            if(skill_hash[skill[i]] == 0) continue;
+            --skill_hash[skill[i]];
+            if(skill_hash[target - skill[i]] == 0)
+                return -1;
+            --skill_hash[target - skill[i]];
+            ans += (skill[i] * (target - skill[i]));
+        }
+        return ans;
+    }
+};
